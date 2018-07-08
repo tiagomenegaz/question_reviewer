0d5ad4f45709d0ec5b07d422706abf7b56af305d
Tiago Garcia <tiago.tiago22@gmail.com>
2018-07-08 18:00:34 -0300

    Add test to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should created user not to be admin" do
+    user = User.create(email: "test@exemple.com", password: "App123")
+    assert !user.admin?
+  end
+
+  test "should created user to be admin" do
+    user = User.create(email: "test@exemple.com", password: "App123", admin: true)
+    assert user.admin?
+  end
 end

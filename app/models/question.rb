24cfe20f9e35c760071e1e63820c247182a16c00
Tiago Garcia <tiago.tiago22@gmail.com>
2018-07-08 19:24:03 -0300

    Add validation to question fields

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :user
 
+  validates :content, :source, :year, presence: true
+
   scope :pending, -> { where.not(pending_at: nil) }
   scope :approved, -> { where.not(approved_at: nil) }
   scope :reproved, -> { where.not(reproved_at: nil) }

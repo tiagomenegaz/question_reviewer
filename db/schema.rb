ab6e6d1cff94e6cd87abaf90bb39e94ff67604b6
Tiago Garcia <tiago.tiago22@gmail.com>
2018-07-08 17:38:35 -0300

    Add questions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_08_202635) do
+ActiveRecord::Schema.define(version: 2018_07_08_203318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.text "content"
+    t.string "source"
+    t.date "year"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +43,5 @@ ActiveRecord::Schema.define(version: 2018_07_08_202635) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end

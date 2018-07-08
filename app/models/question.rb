d1af7a802724312b2e4e125be8a4ab68bd1c6639
Tiago Garcia <tiago.tiago22@gmail.com>
2018-07-08 19:03:06 -0300

    Add scopes to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
+
+  scope :pending, -> { where.not(pending_at: nil) }
+  scope :approved, -> { where.not(approved_at: nil) }
+  scope :reproved, -> { where.not(reproved_at: nil) }
 end

fd3ea3dda3d565c0886ba2ee299fa890681a261f
Tiago Garcia <tiago.tiago22@gmail.com>
2018-07-08 19:57:55 -0300

    Add state constraint to questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_08_215846) do
+ActiveRecord::Schema.define(version: 2018_07_08_225206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
